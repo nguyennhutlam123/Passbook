4dd6057c1c567edbd2b42ff9cb4300dc70566378
nguyenhuulap <nguyennhutlam9a3@gmail.com>
2026-09-21 02:26:24 +0700

    Update api.js

--- python/frontend/js/api.js
+++ python/frontend/js/api.js
@@ -1,4 +1,5 @@
-const API_BASE_URL = window.PASSBOOK_API_URL || "http://127.0.0.1:8000/api";
+const API_BASE_URL = window.PASSBOOK_API_URL
+    || "https://passbook-backend-6o0s.onrender.com/api";
 
 function getAccessToken() {
     return localStorage.getItem("accessToken");
